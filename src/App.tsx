778d7c25e95f92f9bd8643cdebc2109ec2bc721c
Roman <spiderr.roma@gmail.com>
2022-02-07 23:27:41 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return <div className='header-yellow'>
+    <div className='eventor-name'> Eventor 
+    <div className='serchbar'> Searchbar-mock</div>
+    <div> Choose-city-mock</div>
+    
+    </div>
+  </div>;
+}
+
+export default App;
