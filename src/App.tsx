f07f4fd79ffe67dc21671f662210f74e6cb13423
Roman <spiderr.roma@gmail.com>
2022-02-15 22:48:09 +0200

    smth

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css'
+import Header from './Header';
+
 
 function App() {
   return <div className='main-app'>
-    {/* <Header /> */}
+    <Header />
     {/* <Events /> */}
     {/* <Selector />     */}
     </div>
