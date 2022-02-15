c0e80e8e6179998c5bad9a0f78597558d3d831ae
Roman <spiderr.roma@gmail.com>
2022-02-15 22:21:08 +0200

    added whatever

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css'
 
 function App() {
-  return <div className='header-yellow'>
-    <div className='eventor-name'> Eventor 
-    <div className='serchbar'> Searchbar-mock</div>
-    <div> Choose-city-mock</div>
-    
+  return <div className='main-app'>
+    {/* <Header /> */}
+    {/* <Events /> */}
+    {/* <Selector />     */}
     </div>
-  </div>;
 }
 
 export default App;
